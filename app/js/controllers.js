b3177795cb821004d844853b827c0dd0a1091222
Gordon Zhu <gordonmzhu@gmail.com>
2015-06-19 11:10:26 -0700

    Cleanup and setup for landing page.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
+angular.module('myApp.controllers', [])
+  .controller('LandingPageController', [function() {
 
   }]);
