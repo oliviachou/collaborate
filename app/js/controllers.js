760ceaa1cd554e2b2e81e3293a08748c8ddf564d
Gordon Zhu <gordonmzhu@gmail.com>
2015-06-27 12:23:17 -0700

    Modified WaitlistController to save parties to /parties, wrote sendTextMessage method on WaitlistController.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,18 +7,29 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('WaitlistController', ['$firebaseArray', function($firebaseArray) {
-    var partiesReference = new Firebase('https://gz-angularfire-test.firebaseio.com/');
-
+    // Connect parties to Firebase.
+    var partiesReference = new Firebase('https://gz-angularfire-test.firebaseio.com/parties');
     this.parties = $firebaseArray(partiesReference);
 
+    // Object to store data from the waitlist form.
     this.newParty = {
       name: '',
       phone: '',
       size: ''
     };
 
+    // Function to save a new party to the waitlist.
     this.saveParty = function() {
       this.parties.$add(this.newParty);
       this.newParty = {};
+    };
+
+    // Function to send a text message to a party.
+    this.sendTextMessage = function(phoneNumber) {
+      var textMessagesReference = new Firebase('https://gz-angularfire-test.firebaseio.com/textMessages');
+      var textMessages = $firebaseArray(textMessagesReference);
+      textMessages.$add({
+        phoneNumber: phoneNumber
+      });
     };
   }]);
