bbebf761b3d7695e586b1d3813d323067be0ad05
Gordon Zhu <gordonmzhu@gmail.com>
2015-07-08 15:09:43 -0700

    /waitlist-modular at feature parity with /waitlist.

--- src/app/waitlist/config.route.js
+++ src/app/waitlist/config.route.js
@@ -6,12 +6,19 @@
     .config(appRun);
 
   appRun.$inject = ['$routeProvider'];
+  resolveCurrentUser.$inject = ['authService'];
 
   function appRun($routeProvider) {
     $routeProvider.when('/waitlist-modular', {
       templateUrl: 'app/waitList/waitList.html',
-      controller: 'WaitList'
+      controller: 'WaitList',
+      controllerAs: 'vm',
+      resolve: {currentUser: resolveCurrentUser}
     });
+  }
+
+  function resolveCurrentUser(authService) {
+    return authService.firebaseAuthObject.$requireAuth();
   }
 
 })();
--- src/app/waitlist/waitlist.js
+++ src/app/waitlist/waitlist.js
@@ -5,8 +5,34 @@
     .module('myApp.waitList')
     .controller('WaitList', WaitList);
 
-  function WaitList() {
-    console.log('Inside the WaitList controller.');
+  WaitList.$inject = ['partyService', 'textMessageService', 'currentUser'];
+
+  function WaitList(partyService, textMessageService, currentUser) {
+    // Keep a reference to the controller instance.
+    var waitListController = this;
+
+    // Connect parties to Firebase.
+    waitListController.parties = partyService.getPartiesByUser(currentUser.uid);
+
+    // Object to store data from the waitList form.
+    waitListController.newParty = {
+      name: '',
+      phone: '',
+      size: '',
+      done: false,
+      notified: false
+    };
+
+    // Function to save a new party to the waitList.
+    waitListController.saveParty = function() {
+      partyService.saveParty(waitListController.newParty, currentUser.uid);
+      waitListController.newParty = {};
+    };
+
+    // Function to send a text message to a party.
+    waitListController.sendTextMessage = function(party) {
+      textMessageService.sendTextMessage(party, currentUser.uid, waitListController.parties);
+    };
   }
 
 })();
