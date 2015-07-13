91ff5adc4732a2e5043869e424381123a9cbdc51
Gordon Zhu <gordonmzhu@gmail.com>
2015-07-13 10:29:45 -0700

    Moved auth service to app.auth.

---
+++ src/app/auth/auth.service.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.auth')
+    .factory('authService', authService);
+
+  authService.$inject = ['$rootScope', '$firebaseAuth', 'firebaseData'];
+
+  function authService($rootScope, $firebaseAuth, firebaseData) {
+    var firebaseAuthObject = $firebaseAuth(firebaseData);
+
+    $rootScope.currentUser = null;
+
+    firebaseAuthObject.$onAuth(function(currentUser) {
+      $rootScope.currentUser = currentUser;
+    });
+
+    var authServiceObject = {
+      firebaseAuthObject: firebaseAuthObject,
+      register: function(user) {
+        return firebaseAuthObject.$createUser(user);
+      },
+      login: function(user) {
+        return firebaseAuthObject.$authWithPassword(user);
+      },
+      logout: function() {
+        $rootScope.$broadcast('logout');
+        firebaseAuthObject.$unauth();
+      },
+      getCurrentUser: function() {
+        return $rootScope.currentUser.uid;
+      },
+      sendWelcomeEmail: function(emailAddress) {
+        firebaseData.child('emails')
+          .push({emailAddress: emailAddress});
+      }
+    };
+
+    return authServiceObject;
+  }
+
+})();
--- src/app/services.js
+++ src/app/services.js
@@ -2,39 +2,6 @@
   'use strict';
 
   angular.module('app.services', [])
-    .factory('authService', ['$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL', 'firebaseData',
-      function($rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL, firebaseData) {
-      var firebaseAuthObject = $firebaseAuth(firebaseData);
-
-      $rootScope.currentUser = null;
-
-      firebaseAuthObject.$onAuth(function(currentUser) {
-        $rootScope.currentUser = currentUser;
-      });
-
-      var authServiceObject = {
-        firebaseAuthObject: firebaseAuthObject,
-        register: function(user) {
-          return firebaseAuthObject.$createUser(user);
-        },
-        login: function(user) {
-          return firebaseAuthObject.$authWithPassword(user);
-        },
-        logout: function() {
-          $rootScope.$broadcast('logout');
-          firebaseAuthObject.$unauth();
-        },
-        getCurrentUser: function() {
-          return $rootScope.currentUser.uid;
-        },
-        sendWelcomeEmail: function(emailAddress) {
-          firebaseData.child('emails')
-            .push({emailAddress: emailAddress});
-        }
-      };
-
-      return authServiceObject;
-    }])
     .factory('textMessageService', ['$firebaseArray', 'FIREBASE_URL', 'party', 'firebaseData',
       function($firebaseArray, FIREBASE_URL, party, firebaseData) {
 
