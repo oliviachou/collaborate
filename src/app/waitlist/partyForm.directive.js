493840233d109b5a9072ed2affe6e4414bd0cdda
Gordon Zhu <gordonmzhu@gmail.com>
2015-07-17 22:13:32 -0700

    removed gz prefix from filenames.

---
+++ src/app/waitlist/partyForm.directive.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.waitList')
+    .directive('gzPartyForm', gzPartyForm);
+
+  function gzPartyForm() {
+    return {
+      templateUrl: 'app/waitList/gzPartyForm.html',
+      restrict: 'E',
+      controller: PartyFormController,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+        parties: '='
+      }
+    }
+  }
+
+  function PartyFormController() {
+    var vm = this;
+
+    vm.newParty = {
+      name: '',
+      phone: '',
+      size: '',
+      done: false,
+      notified: false
+    };
+    vm.addParty = addParty;
+
+    function addParty() {
+      vm.parties.$add(vm.newParty);
+      vm.newParty = {};
+    }
+  }
+
+})();
